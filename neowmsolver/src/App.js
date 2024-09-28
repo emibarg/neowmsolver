22bed0afebbb36f4683996b8ff72fab3fed65771
Emiliano Barg <113928403+emibarg@users.noreply.github.com>
2024-09-28 20:08:31 -0300

    added non functional toolbox

--- neowmsolver/src/App.js
+++ neowmsolver/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Grid from './Grid';  // Import the Grid component
+import Toolbox from './Toolbox';
 
 function App() {
   return (
     <div className="App">
+      
       <h1>Structural Beam Calculator with Grid</h1>
+      <Toolbox/>
       <Grid />  {/* Use the Grid component */}
     </div>
   );
